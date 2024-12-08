4f2f5a35a4066e77619daf1854f8a1743159176d
doocman <robin.savonensoderholm@gmail.com>
2024-12-08 21:03:02 +0100

    add linux implementations throuh header files as of now

---
+++ inc/picolinux/picolinux_libc.hpp
@@ -0,0 +1,15 @@
+
+#include "pico/stdlib.h"
+#include "unistd.h"
+
+extern "C" {
+int usleep(useconds_t us) {
+    sleep_us(us);
+    return 0;
+}
+unsigned int sleep(unsigned int seconds) {
+    sleep_ms(seconds * 1000);
+    return 0;
+}
+}
+
--- src/picolinuxc.cpp
+++ src/picolinuxc.cpp
@@ -1,12 +1,2 @@
 
-#include "pico/stdlib.h"
-#include "unistd.h"
 
-int usleep(useconds_t us) {
-    sleep_us(us);
-    return 0;
-}
-unsigned int sleep(unsigned int seconds) {
-    sleep_ms(seconds * 1000);
-    return 0;
-}
